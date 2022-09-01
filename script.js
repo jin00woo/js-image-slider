e1988e8620ece24e2da98c8f78d4ccc53d1c330e
Jinwoo Han <56533546+jin00woo@users.noreply.github.com>
2022-09-01 00:58:59 +0000

    Added css elements, added nextSlide and prevSlide function

---
+++ script.js
@@ -0,0 +1,28 @@
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if(current.nextElementSibling){
+    current.nextElementSibling.classList.add("current");
+  }
+  else{
+    slides[0].classList.add("current");
+  }
+}
+
+const prevSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if(current.previousElementSibling){
+    current.previousElementSibling.classList.add("current");
+  }
+  else{
+    slides[slides.length - 1].classList.add("current");
+  }
+}
